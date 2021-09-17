16fa536003ec888c36c3f3d8bfc7c94aa49acd19
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 23:50:50 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+
+fs.readFile('./sheetData.txt', (err, content) => {
+    if(err) { console.error(`${err.message}`); return }
+    console.log(content.toString('utf8'));
+})
