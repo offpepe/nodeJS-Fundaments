627929a4be6d946d8b1f484350b5168c54ab6adb
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-16 23:54:33 -0400

    feat requiring fs and usi w/callback

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const fs = require('fs');
 fs.readFile('./sheetData.txt', (err, content) => {
     if(err) { console.error(`${err.message}`); return }
     console.log(content.toString('utf8'));
-})
+});
