a4874815ea0d92724c29c8c01e3448dde7812e55
Alan Albuquerque F. Lopes <alan.alb.flopes@gmail.com>
2021-09-17 12:38:23 -0400

    feat: txt archive string type conversor w/fs

--- exercices/fs-stringConversor.js
+++ exercices/fs-stringConversor.js
@@ -1,10 +1,15 @@
 const fs = require('fs').promises;
 
 const stringConversor = async (file, content, type) => {
+    try {
     const readedContent = await fs.readFile(content, 'utf-8');
     const operation = await fs.writeFile(file, readedContent, type);
     const operationResult = operation;
     console.log(operationResult);
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 module.exports = stringConversor;
